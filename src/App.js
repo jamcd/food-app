c7e7a504f2ea75925be13cdb93ffb24ae80d5baf
Jamie M <jamiemcd13@gmail.com>
2018-02-24 21:35:47 +0000

    Refactored parts of the main App and added some Bulma style overrides

--- src/App.js
+++ src/App.js
@@ -7,36 +7,39 @@ import jelly from './img/food/jelly.svg'
 import apple from './img/food/apple.svg'
 import bacon from './img/food/bacon.svg'
 import banana from './img/food/banana.svg'
+import FoodItem from './components/FoodItem'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <section className="section">
-          <Header />
-          <SubHeader />
-        </section>
+        <NavBar />
+        <Header />
+        <HomePage />
       </div>
     );
   }
 }
 
-class Header extends Component {
-  render () {
+class NavBar extends Component {
+  render() {
     return (
-      <header className="site-header cf">
+      <section className="navbar" role="navigation">
         <div className="container">
-          <div className="site-logo">
-            <h1>
+          <div className="navbar-brand">
+            <a href="/" className="navbar-item">
               <span className="title-span--food">food</span><span className="title-span--me">me</span><span
               className="title-span--up">up</span>
-            </h1>
+            </a>
           </div>
-          <div className="site-nav cf">
-            <div className="site-nav__item">Home</div>
-            <div className="site-nav__item">Food</div>
-            <div className="site-nav__item">Planner
+          <div className="navbar-menu navbar-end">
+            <div className="site-nav-item">Home</div>
+
+            <div className="site-nav-item">Food</div>
+
+            <div className="site-nav-item">Planner
+
               <div className="site-nav__submenu">
                 <div className="food-item col-jm-8" draggable="true">
                   <div className="food-item__heading">
@@ -74,79 +77,79 @@ class Header extends Component {
                   </div>
                 </div>
               </div>
-
             </div>
           </div>
-
-          <HomePage />
         </div>
-      </header>
+      </section>
+    )
+  }
+}
+
+class FeaturedFoods extends Component {
+  render() {
+    return [
+      <div className="column">
+        <FoodItem name="Carrot" imgUri={carrot} />
+      </div>,
+      <div className="column">
+        <FoodItem name="Apple" imgUri={apple} />
+      </div>,
+      <div className="column">
+        <FoodItem name="Bacon" imgUri={bacon} />
+      </div>,
+      <div className="column">
+        <FoodItem name="Banana" imgUri={banana} />
+      </div>
+    ]
+  }
+}
+
+class Header extends Component {
+  render () {
+    return (
+      <section className="hero">
+        <div className="hero-body welcome-section">
+          <div className="container">
+            <h1 className="title welcome-title">Hello #NAME#</h1>
+            <h2 className="subtitle welcome-message">Welcome back to foodmeup! Here are your most recently eaten foods...</h2>
+          </div>
+        </div>
+      </section>
     );
   }
 }
 
-function SubHeader (props) {
-  return (
-    <main className="site-main">
-      <div className="site-section welcome-section cf">
-        <div className="container">
-          <h2 className="welcome-title">Hello Jamie!</h2>
-          <h4 className="welcome-message">Welcome back to foodmeup! Here are your most recently eaten foods...</h4>
-        </div>
-      </div>
-    </main>
-  );
-}
+// function SubHeader (props) {
+//   return (
+//
+//   );
+// }
 
 class HomePage extends Component {
   render () {
-    return (
-      <div className="site-section cf">
+    return [
+      <section className="section">
         <div className="container">
           <div className="columns">
-
-            <div className="food-item column" draggable="true">
-              <div className="food-item__heading">
-                <h6>Carrot</h6>
-              </div>
-              <div className="food-item__content">
-                <img className="food-item__image" src={carrot} alt="Carrot" width="50" height="50" draggable="false" />
-              </div>
+            <FeaturedFoods />
+            <div className="column is-half">
+              <ShoppingList />
             </div>
-
-            <div className="food-item column" draggable="true">
-              <div className="food-item__heading">
-                <h6>Apple</h6>
-              </div>
-              <div className="food-item__content">
-                <img className="food-item__image" src={apple} alt="Apple" width="50" height="50" draggable="false" />
-              </div>
-            </div>
-
-            <div className="food-item column" draggable="true">
-              <div className="food-item__heading">
-                <h6>Bacon</h6>
-              </div>
-              <div className="food-item__content">
-                <img className="food-item__image" src={bacon} alt="Bacon" width="50" height="50" draggable="false" />
-              </div>
-            </div>
-
-            <div className="food-item food-item--added column" draggable="true">
-              <div className="food-item__heading">
-                <h6>Banana</h6>
-              </div>
-              <div className="food-item__content">
-                <img className="food-item__image" src={banana} alt="Banana" width="50" height="50" draggable="false" />
-              </div>
-            </div>
-
-            <ShoppingList />
-
           </div>
         </div>
-      </div>
-    );
+      </section>,
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half">
+              <div className="columns">
+                <FeaturedFoods />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    ];
   }
 }
 
@@ -175,9 +178,9 @@ class ShoppingList extends Component {
       <div className="shopping-list__container">
         <div className="shopping-list">
           <div className="shopping-list__heading">
-            <h6>Shopping List</h6>
+            <h6 className="title is-6">Shopping List</h6>
           </div>
-          <div className="shopping-list__content cf">
+          <div className="shopping-list__content is-clearfix">
             <p>We have generated a shopping list for you to make your recipes. Remember to delete the items that you don't need.</p>
 
             <h6>Fruits & Vegetables</h6>
@@ -185,7 +188,7 @@ class ShoppingList extends Component {
               {this.foodToBuy.fruitVeg.map(food => {
                 return (
                   <div className="col-jm-2" key={food}>
-                    <ul className="cf">
+                    <ul className="is-clearfix">
                       <li className={'shopping-list__item food-icon--' + food.toLowerCase()}>{food}
                         <span className="shopping-list__item-weight">10mg</span>
                       </li>
@@ -200,7 +203,7 @@ class ShoppingList extends Component {
               {this.foodToBuy.meat.map(food => {
                 return (
                   <div className="col-jm-2" key={food}>
-                    <ul className="cf">
+                    <ul className="is-clearfix">
                       <li className={'shopping-list__item food-icon--' + food.toLowerCase()}>{food}
                         <span className="shopping-list__item-weight">10mg</span>
                       </li>
@@ -213,7 +216,7 @@ class ShoppingList extends Component {
             <h6>Already got these</h6>
             <div className="row">
               <div className="col-jm-1">
-                <ul className="cf">
+                <ul className="is-clearfix">
                   <li className="shopping-list__item food-icon--pepper">Pepper
                     <span className="shopping-list__item-weight">10mg</span>
                   </li>
