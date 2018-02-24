3d59d33c11800e4e4f7568b06abdc13ed97404dc
Jamie M <jamiemcd13@gmail.com>
2018-02-24 14:10:52 +0000

    Fixing setup to use Sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <section className="section">
-          <div className="container">
+        <div className="container">
             <h1 className="title">
               Hello World
             </h1>
@@ -18,7 +18,7 @@ class App extends Component {
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <h1 className="App-title">Welcome to React</h1>
-            </header>
+      </header>
             <p className="App-intro">
               To get started, edit <code>src/App.js</code> and save to reload.
             </p>
