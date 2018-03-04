c11ff4245bb283bd461810d59bd05a8ccd412504
Jamie M <jamiemcd13@gmail.com>
2018-03-04 12:46:49 +0000

    Removed redundant code from index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-import { addFood, removeFood } from './actions/food'
-import FoodItems from './components/FoodItems'
-
 let store = createStore(foodApp)
 
 ReactDOM.render(
@@ -19,23 +16,3 @@ ReactDOM.render(
   document.getElementById('root')
 )
 registerServiceWorker()
-
-// Log the initial state
-console.log(store.getState())
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-)
-
-// Dispatch some actions
-store.dispatch(addFood(FoodItems.apple, 2))
-store.dispatch(addFood(FoodItems.eggs, 5))
-store.dispatch(addFood(FoodItems.cake, 10))
-store.dispatch(addFood(FoodItems.meat, 15))
-store.dispatch(removeFood('Apple'))
-store.dispatch(removeFood('Eggs'))
-
-// Stop listening to state updates
-unsubscribe()
